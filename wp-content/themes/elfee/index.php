043f15831fd9764248ba1efcfec7c7b8db1bd161
Orianne Cielat <orianne.cielat@gmail.com>
2023-01-19 15:52:14 +0100

    début de construction page d'accueil (index.php)

--- wp-content/themes/elfee/index.php
+++ wp-content/themes/elfee/index.php
@@ -1,7 +1,21 @@
 <?php
     get_header();
 ?>
-Test
+
+<?php
+if(have_posts()) :
+?>
+<?php while(have_posts()) : the_post(); ?>
+    <article>
+        <h2 class="title_homepage"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+        <div style="width: 100%;"><?php the_post_thumbnail('full', ['style' => 'height: 400px; object-fit: cover;']) ?></div>
+    </article>
+<?php endwhile; ?>
+
+<?php else : ?>
+    <h1>Pas d'articles :(</h1>
+<?php endif; ?>
+
 <?php
     get_footer();
 ?>
