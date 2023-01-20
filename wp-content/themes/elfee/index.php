da8c544ad05cc9056cb26aab6a445abb9872d0e9
Orianne Cielat <orianne.cielat@gmail.com>
2023-01-20 20:17:15 +0100

    mise à jour images/gestion titres homepage

--- wp-content/themes/elfee/index.php
+++ wp-content/themes/elfee/index.php
@@ -7,11 +7,12 @@ if(have_posts()) :
 ?>
 <?php while(have_posts()) : the_post(); ?>
     <article>
-        <?php get_img() ?>
+        <?php elfee_get_img() ?>
         <!-- <img src="<?php //get_template_directory_uri() ?>./wp-content/themes/elfee/assets/img/bol.png" style="border: 3px solid black; width:100px; height:100px;">
         <img src="<?php //get_template_directory() ?>./wp-content/themes/elfee/assets/img/bol.png" style="border: 3px solid black; width:100px; height:100px;"> -->
-        <h2 class="title_homepage"><a class="a_title_homepage" href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+        <?php elfee_get_title_homepage() ?>
         <div style="width: 100%;"><?php the_post_thumbnail('full', ['style' => 'height: 400px; object-fit: cover;']) ?></div>
+        <div class="card_homepage"><?php the_content() ?></div>
     </article>
 <?php endwhile; ?>
 
