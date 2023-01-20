7427a30477564219f3aa0c27a720d7c956f21889
Orianne Cielat <orianne.cielat@gmail.com>
2023-01-20 16:52:20 +0100

    homepage/création fonction insertion images/css

--- wp-content/themes/elfee/index.php
+++ wp-content/themes/elfee/index.php
@@ -7,7 +7,10 @@ if(have_posts()) :
 ?>
 <?php while(have_posts()) : the_post(); ?>
     <article>
-        <h2 class="title_homepage"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+        <?php get_img() ?>
+        <!-- <img src="<?php //get_template_directory_uri() ?>./wp-content/themes/elfee/assets/img/bol.png" style="border: 3px solid black; width:100px; height:100px;">
+        <img src="<?php //get_template_directory() ?>./wp-content/themes/elfee/assets/img/bol.png" style="border: 3px solid black; width:100px; height:100px;"> -->
+        <h2 class="title_homepage"><a class="a_title_homepage" href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
         <div style="width: 100%;"><?php the_post_thumbnail('full', ['style' => 'height: 400px; object-fit: cover;']) ?></div>
     </article>
 <?php endwhile; ?>
