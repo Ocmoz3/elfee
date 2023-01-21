d755dd4e1f94befaaefb4927f6009ac8a693b94f
Orianne Cielat <orianne.cielat@gmail.com>
2023-01-21 13:55:13 +0100

    mise en page cards homepage

--- wp-content/themes/elfee/index.php
+++ wp-content/themes/elfee/index.php
@@ -12,7 +12,14 @@ if(have_posts()) :
         <img src="<?php //get_template_directory() ?>./wp-content/themes/elfee/assets/img/bol.png" style="border: 3px solid black; width:100px; height:100px;"> -->
         <?php elfee_get_title_homepage() ?>
         <div style="width: 100%;"><?php the_post_thumbnail('full', ['style' => 'height: 400px; object-fit: cover;']) ?></div>
-        <div class="card_homepage"><?php the_content() ?></div>
+        <div class="card_homepage">
+            <?php the_content() ?>
+            <?php if(get_the_ID() == 7): ?>
+            <button class="button btn-massage">En savoir plus</button>
+            <?php elseif((get_the_ID() == 5)): ?>
+            <button class="button btn-reflexology">En savoir plus</button>
+            <?php endif; ?>
+        </div>
     </article>
 <?php endwhile; ?>
 
