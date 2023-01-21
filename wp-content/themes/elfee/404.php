083883b1c8ec7f2b0887336d71309166aa79eb5a
Orianne Cielat <orianne.cielat@gmail.com>
2023-01-21 14:57:13 +0100

    création page 404

---
+++ wp-content/themes/elfee/404.php
@@ -0,0 +1,16 @@
+<?php
+get_header()
+?>
+
+<div class="div-404">
+    <!-- <div> -->
+        <h1>Page introuvable</h1>
+        <p>
+        Cette page n'existe pas
+        </p>
+    <!-- </div> -->
+</div>
+    
+<?php
+get_footer()
+?>
