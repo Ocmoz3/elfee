2a71c698c332c3c855aa452ef16476cecce61ac6
Orianne Cielat <orianne.cielat@gmail.com>
2023-01-25 04:01:23 +0100

    mise en place walker pour images menu

--- wp-content/themes/elfee/header.php
+++ wp-content/themes/elfee/header.php
@@ -38,100 +38,14 @@
             'container' => 'nav',
             'container_class' => 'nav_header',
             'menu_class' => 'ul_menu_header',
-            'link_after' => '<img src="' . get_template_directory_uri() . '/assets/img/' . get_nav_icon() . '" alt="" style="width: 50px; margin: 0 auto;">'
             // 'link_after' => '<img src="'. get_template_directory_uri() . '/assets/img/' . $icon . '" alt="" style="width: 50px; margin: 0 auto;">'
-            // 'link_after' => get_nav_icon()
-            // 'link_after' => get_menu_item_ID()
-            // 'link_after' => get_menu_item_ID()
+            // 'link_after' => test()
+            'walker' => new Menu_With_Description
         ]) ?>
         <?php
 
-        function get_nav_icon()
-        {
-
-            // $itemID = get_menu_item_ID();
-            // echo $itemID;
-            $links = wp_get_menu_array('navigation');
-            echo '<hr>';
-            echo '<hr>';
-            // var_dump($links);
-            echo '<hr>';
-            echo '<hr>';
-            // if ($itemID == 31) {
-            $array = [];
-            foreach ($links as $link) {
-                $array[] = $link['ID'];
-                // $array[$link->ID]['ID'] = $link['ID'];
-                // echo $link['ID'];
-                var_dump($array);
-                // return $link['ID'];
-            }
-
-            if ($link['ID'] == 24) {
-                $icon = 'icone_accueil.png';
-                echo $icon;
-            } elseif ($link['ID'] == 31) {
-                $icon = 'reflexology.png';
-            }
-            // return $navItem->ID;
-            return $icon;
-        }
-
-        function get_menu_item_ID()
-        {
-            $menuLocations = get_nav_menu_locations(); // Get our nav locations (set in our theme, usually functions.php)
-            // This returns an array of menu locations ([LOCATION_NAME] = MENU_ID);
-            $menuID = $menuLocations['header']; // Get the *primary* menu ID
-            $primaryNav = wp_get_nav_menu_items($menuID); // Get the array of wp objects, the nav items for our queried location.
-            foreach ($primaryNav as $navItem) {
-                // echo $navItem->ID;
-                echo $navItem->title;
-                // return $navItem->title;
-                // return $navItem->ID;
-
-            }
-        }
-
-        // //////////////////////////////////////:
-        function wp_get_menu_array($current_menu)
-        {
-
-            $array_menu = wp_get_nav_menu_items($current_menu);
-            $menu = array();
-            foreach ($array_menu as $m) {
-                if (empty($m->menu_item_parent)) {
-                    $menu[$m->ID] = array();
-                    $menu[$m->ID]['ID'] = $m->ID;
-                    $menu[$m->ID]['title'] = $m->title;
-                    $menu[$m->ID]['url'] = $m->url;
-                    $menu[$m->ID]['children'] = array();
-                }
-            }
-            // $submenu = array();
-            // foreach ($array_menu as $m) {
-            //     if ($m->menu_item_parent) {
-            //         $submenu[$m->ID] = array();
-            //         $submenu[$m->ID]['ID'] = $m->ID;
-            //         $submenu[$m->ID]['title'] = $m->title;
-            //         $submenu[$m->ID]['url'] = $m->url;
-            //         $menu[$m->menu_item_parent]['children'][$m->ID] = $submenu[$m->ID];
-            //     }
-            // }
-            return $menu;
-        }
-        
         ?>
 
     </div>
-    <?php
-    echo'<hr>';
-    echo'<hr>';
-    $a = wp_get_menu_array('navigation');
-    // print_r($a);
-    // exit;
-    echo'<hr>';
-    // echo $a['24']['ID'];
-    // get_nav_icon()
-    ?>
 
     <div class="wrap">
