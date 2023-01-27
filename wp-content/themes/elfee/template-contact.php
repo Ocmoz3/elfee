23f040bc860b3ff3b8addf7ca71297aa343c7572
Orianne Cielat <orianne.cielat@gmail.com>
2023-01-27 15:40:17 +0100

    création fichier class walker nav/màj CSS

---
+++ wp-content/themes/elfee/template-contact.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* Template Name: Page Contact
+ */
+?>
+
+<?php
+    get_header();
+?>
+
+<div class="div-logo">
+    <img src="<?php echo get_theme_file_uri() ?>/assets/img/icone_contact_without_background.png" alt="">
+</div>
+
+<!-- ICI
+INSÉRER FORMULAIRE DE CONTACT
+-->
+
+<!-- Voir si on doit intégrer NONCE pour la sécurité
+Voir fin de vidéo sur les métadonnées -->
+
+<?php
+    get_footer();
+?>
