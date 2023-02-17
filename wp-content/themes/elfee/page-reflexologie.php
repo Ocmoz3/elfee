8099a200cb622489323643ce8624746a8661bb93
Orianne Cielat <orianne.cielat@gmail.com>
2023-02-17 17:30:36 +0100

    mise en place card page

--- wp-content/themes/elfee/page-reflexologie.php
+++ wp-content/themes/elfee/page-reflexologie.php
@@ -3,7 +3,6 @@
 ?>
 <?php
 ?>
-<!-- <h1>PAGE-REFLEXOLOGY</h1> -->
 <div class="div_icone_page">
     <!-- <img src="<?php //echo get_theme_file_uri() ?>/assets/img/reflexology.png" alt=""> -->
     <img src="<?php echo get_field('icone') ?>" alt="">
@@ -20,7 +19,6 @@
 </div>
 
 <div>
-    
     <?php $titre_description = get_field('titre_description');
     if($titre_description): ?>
     <div class="page_background">
@@ -30,12 +28,26 @@
             <div class="page_description"><?php echo $titre_description['description'] ?></div>
         <!-- </div> -->
     </div>
+
     <style type="text/css">
         .page_background::before {
             background:url("<?php echo esc_url($titre_description['image_background']); ?>") 0 0 no-repeat;
             }
     </style>
     <?php endif; ?>
+</div>
+
+<div class="page_card" style="border: 3px solid green;">
+    <div class="div_main_page_card" style="border: 3px solid yellow;">
+        <div class="div_img_page_card" style="border: 3px solid red;">
+            <img class="img_page_card" src="<?= get_field('image_card') ?>" alt="">
+        </div>
+        <div class="div_text_page_card">
+            <h4 class="h4_text_page_card">Réflexologie plantaire et palmaire</h4>
+            <p class="p_desc_text_page_card">Massage de zones réflexes des pieds et les mains à partir d’un objectif défini ensemble</p>
+            <p class="p_time_text_page_card">1h - plus court pour les enfants</p>
+        </div>
+    </div>
 </div>
 
 <?php
