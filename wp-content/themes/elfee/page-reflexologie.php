676b10dd9143d9339aa4958ce3dcfca660f6449e
Orianne Cielat <orianne.cielat@gmail.com>
2023-02-18 19:19:25 +0100

    page réflexologie

--- wp-content/themes/elfee/page-reflexologie.php
+++ wp-content/themes/elfee/page-reflexologie.php
@@ -1,53 +1,79 @@
 <?php
     get_header();
 ?>
-<?php
-?>
+
 <div class="div_icone_page">
-    <!-- <img src="<?php //echo get_theme_file_uri() ?>/assets/img/reflexology.png" alt=""> -->
-    <img src="<?php echo get_field('icone') ?>" alt="">
+    <img src="<?php echo esc_url(get_field('icone')); ?>" alt="">
 </div>
-<div>
-    <!-- <p class="quote">“Tu ne peux pas voyager sur un chemin sans être toi-même le chemin.”<br> -->
-    <!-- Bouddha</p> -->
-    <p class="quote">“<?php echo get_field('citation') ?>”<br>
-    <?php echo get_field('auteur_citation') ?></p>
+<div class="div_quote">
+    <p class="quote">“<?php echo esc_html(get_field('citation')); ?>”<br>
+    <?php echo esc_html(get_field('auteur_citation')); ?></p>
 </div>
 <div class="div-chakras">
-    <!-- <img src="<?php //echo get_template_directory_uri() ?>/assets/img/chakras.png" alt=""> -->
-    <img src="<?php echo get_field('separateur') ?>" alt="">
+    <img src="<?php echo esc_url(get_field('separateur')); ?>" alt="">
 </div>
 
 <div>
     <?php $titre_description = get_field('titre_description');
     if($titre_description): ?>
     <div class="page_background">
-        <!-- <div class="div_absolute"> -->
-            <h2 class="title_homepage"><a class="a_title_homepage title_reflexology" href="<?php the_permalink() ?>"><?php echo $titre_description['titre'] ?></a></h2>
-            <h3 class="page_subtitle"><?php echo $titre_description['sous-titre'] ?></h3>
-            <div class="page_description"><?php echo $titre_description['description'] ?></div>
-        <!-- </div> -->
+            <h2 class="title_homepage"><a class="a_title_homepage title_reflexology" href="<?php the_permalink() ?>"><?php echo esc_html($titre_description['titre']); ?></a></h2>
+            <h3 class="page_subtitle"><?php echo $titre_description['sous-titre']; ?></h3>
+            <div class="page_description"><?php echo $titre_description['description']; ?></div>
     </div>
 
     <style type="text/css">
         .page_background::before {
-            background:url("<?php echo esc_url($titre_description['image_background']); ?>") 0 0 no-repeat;
-            }
+            background: url("<?php echo esc_url($titre_description['image_background']); ?>") 0 0 no-repeat;
+            background-size: cover;
+        }
     </style>
     <?php endif; ?>
 </div>
 
-<div class="page_card" style="border: 3px solid green;">
-    <div class="div_main_page_card" style="border: 3px solid yellow;">
-        <div class="div_img_page_card" style="border: 3px solid red;">
-            <img class="img_page_card" src="<?= get_field('image_card') ?>" alt="">
+<div class="page_card card_reflexology">
+<?php if(have_rows('card')): ?>
+    <?php while(have_rows('card')): the_row()?>
+    <div class="div_main_page_card">
+        <div class="div_img_page_card">
+            <img class="img_page_card" src="<?= get_sub_field('image_card') ?>" alt="">
         </div>
         <div class="div_text_page_card">
-            <h4 class="h4_text_page_card">Réflexologie plantaire et palmaire</h4>
-            <p class="p_desc_text_page_card">Massage de zones réflexes des pieds et les mains à partir d’un objectif défini ensemble</p>
-            <p class="p_time_text_page_card">1h - plus court pour les enfants</p>
+            <!-- <h4 class="h4_text_page_card">Réflexologie plantaire et palmaire</h4> -->
+            <h4 class="h4_text_page_card"><?= get_sub_field('titre_card') ?></h4>
+            <!-- <p class="p_desc_text_page_card">Massage de zones réflexes des pieds et les mains à partir d’un objectif défini ensemble</p> -->
+            <p class="p_desc_text_page_card"><?= get_sub_field('description_card') ?></p>
+            <!-- <p class="p_time_text_page_card">1h - plus court pour les enfants</p> -->
+            <p class="p_time_text_page_card"><?= get_sub_field('temps_card') ?></p>
         </div>
     </div>
+    <?php endwhile; ?>
+<?php endif; ?>
+</div>
+
+<div class="div_listing_benefits">
+    <?php $listing_bienfaits = get_field('listing_bienfaits');
+    if($listing_bienfaits): ?>
+    <div class="listing_benefits_background">
+        <h3 class="title_benefits"><?php echo $listing_bienfaits['titre_bienfaits'] ?></h3>
+        <div class="list_benefits"><?php echo $listing_bienfaits['liste_bienfaits'] ?></div>
+    </div>
+    <style type="text/css">
+        .listing_benefits_background {
+            background-color: <?php echo esc_attr($listing_bienfaits['couleur_de_fond']); ?>;
+        }
+    </style>
+    <?php endif; ?>
+</div>
+
+<div class="div_illustration">
+<?php $illustration = get_field('illustration');
+    if($illustration): ?>
+    <div class="illustration">
+        <img src="<?= esc_url($illustration['image_illustration']) ?>" alt="">
+    </div>
+    <div class="caption"><?= $illustration['legende_illustration'] ?></div>
+    <?php endif; ?>
 </div>
 
 <?php
--- wp-content/themes/elfee/single-post-massage-sonore-aux-bols-tibetains.php
+++ wp-content/themes/elfee/single-post-massage-sonore-aux-bols-tibetains.php
@@ -1,5 +1,5 @@
 <?php
-    get_header();
+get_header();
 ?>
 <img src="<?= get_field('icone') ?>" alt="" style="width: 20px; height: auto;">
 
@@ -8,38 +8,69 @@
 </div>
 <div>
     <p class="quote">“Soyez à vous-même votre propre refuge. Soyez à vous-même votre propre lumière.”<br>
-    Bouddha</p>
+        Bouddha</p>
 </div>
 <div class="div-chakras">
     <img src="<?php echo get_template_directory_uri() ?>/assets/img/chakras.png" alt="">
 </div>
 <?php
-if(have_posts()) :
+if (have_posts()) :
 ?>
-<?php while(have_posts()) : the_post(); ?>
-    <article>
-        <?php elfee_get_title_homepage() ?>
-        <div class="card_homepage">
-            <?php the_content() ?>
-        </div>
-        <div style="display: flex; gap: 1rem;">
-            <div style="width: 30%;">
-                <img src="<?= get_field('detente_sonore') ?>" alt="" style="width: 100%; height: auto;">
+    <?php while (have_posts()) : the_post(); ?>
+        <article>
+            <?php elfee_get_title_homepage() ?>
+            <div class="card_homepage">
+                <?php the_content() ?>
+            </div>
+            <div style="display: flex; gap: 1rem;">
+                <div style="width: 30%;">
+                    <img src="<?= get_field('detente_sonore') ?>" alt="" style="width: 100%; height: auto;">
+                </div>
+                <div>
+                    <img src="<?= get_field('test_img') ?>" alt="" style="width: 100%; height: auto;">
+                </div>
+                <div>
+                    <img src="<?= get_field('test_img') ?>" alt="" style="width: 100%; height: auto;">
+                </div>
             </div>
             <div>
-                <img src="<?= get_field('test_img') ?>" alt="" style="width: 100%; height: auto;">
+                <?php $titre_description = get_field('titre_description');
+                if ($titre_description) : ?>
+                    <div class="page_background">
+                        <!-- <div class="div_absolute"> -->
+                        <h2 class="title_homepage"><a class="a_title_homepage title_reflexology" href="<?php the_permalink() ?>"><?php echo $titre_description['titre'] ?></a></h2>
+                        <h3 class="page_subtitle"><?php echo $titre_description['sous-titre'] ?></h3>
+                        <div class="page_description"><?php echo $titre_description['description'] ?></div>
+                        <!-- </div> -->
+                    </div>
+
+                    <style type="text/css">
+                        .page_background::before {
+                            background: url("<?php echo esc_url($titre_description['image_background']); ?>") 0 0 no-repeat;
+                            background-size: cover;
+                        }
+                    </style>
+                <?php endif; ?>
             </div>
-            <div>
-                <img src="<?= get_field('test_img') ?>" alt="" style="width: 100%; height: auto;">
+            <div class="page_card card_massage" style="border: 3px solid green;">
+                <div class="div_main_page_card" style="border: 3px solid yellow;">
+                    <div class="div_img_page_card" style="border: 3px solid red;">
+                        <img class="img_page_card" src="<?= get_field('image_card') ?>" alt="">
+                    </div>
+                    <div class="div_text_page_card">
+                        <h4 class="h4_text_page_card">Réflexologie plantaire et palmaire</h4>
+                        <p class="p_desc_text_page_card">Massage de zones réflexes des pieds et les mains à partir d’un objectif défini ensemble</p>
+                        <p class="p_time_text_page_card">1h - plus court pour les enfants</p>
+                    </div>
+                </div>
             </div>
-        </div>
-    </article>
-<?php endwhile; ?>
+        </article>
+    <?php endwhile; ?>
 
 <?php else : ?>
     <h1>Pas d'articles :(</h1>
 <?php endif; ?>
 
 <?php
-    get_footer();
+get_footer();
 ?>
