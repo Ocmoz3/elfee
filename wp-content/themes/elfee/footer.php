b2ce0787a396bf0d10368efdc0c433ba23e0ddc1
Orianne Cielat <orianne.cielat@gmail.com>
2023-02-20 17:15:29 +0100

    conception footer (CSS, fonctions)

--- wp-content/themes/elfee/footer.php
+++ wp-content/themes/elfee/footer.php
@@ -1,14 +1,14 @@
         </div>
 
-        <div class="footer" style="background-color: black;">
-            <div class="footer_main" style="display: flex;">
-                <div class="logo_title" style="display: flex; align-items: center; font-family: 'Karma', serif; gap: .5em;">
-                    <div class="div_logo_footer" style="width: 90px;">
+        <div class="footer">
+            <div class="footer_main">
+                <div class="logo_title">
+                    <div class="div_logo_footer">
                         <img src="<?= get_template_directory_uri() ?>/assets/img/logo.png" alt="">
                     </div>
-                    <div style="font-weight: 600;">
-                        <p style="font-size: 1.5em; margin-block-end: -5px;">Elfée Morgane</p>
-                        <p style="margin-block-end: -5px;">Réflexologue</p>
+                    <div class="div_name_footer">
+                        <p class="p_name_footer">Elfée Morgane</p>
+                        <p class="p_works_footer">Réflexologue</p>
                         <p>Praticienne en massages sonores</p>
                     </div>
                 </div>
@@ -21,7 +21,7 @@
                     'theme_location' => 'footer_main',
                     'container' => 'nav',
                     // 'container_class' => 'nav_header',
-                    // 'menu_class' => 'ul_menu_header',
+                    'menu_class' => 'ul_menu_footer',
                     // 'walker' => new Walker_Nav_Menu_Footer
                 ]) ?>
                 </div>
